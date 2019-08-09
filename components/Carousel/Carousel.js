10c001c39c75ed2cb2a7524dc530c25980105112
Mike Harley <mharley12345@gmail.com>
2019-08-09 10:04:46 -0700

    Got the Carousel Working With Issues

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,51 @@
     <div class="right-button"> > </div>
   </div>
 */
+function carouselComponent(){
+  const carouselContainer = document.querySelector("body > div.carousel-container")
+  
+   const carousel=document.createElement('div')
+   carousel.classList.add('carousel')
+   const leftButton = document.createElement('div')
+   leftButton.classList.add('carousel','left-button')
+   const mtn = document.createElement('img')
+   mtn.src="./assets/carousel/mountains.jpeg"
+   const comp = document.createElement('img')
+  comp.src='./assets/carousel/computer.jpeg'
+   const trees = document.createElement('img')
+  trees.src='./assets/carousel/trees.jpeg'
+   const turnTable = document.createElement('img')
+   turnTable.src='./assets/carousel/turntable.jpeg'
+   const rightButton = document.createElement('div')
+   rightButton.classList.add('carousel','right-button')
+   carouselContainer.appendChild(carousel)
+  carousel.appendChild(leftButton)
+  carousel.appendChild(mtn)
+  carousel.appendChild(comp)
+  carousel.appendChild(trees)
+  carousel.appendChild(turnTable)
+  carousel.appendChild(rightButton)
+  const image = document.getElementsByTagName('img')
+  comp.classList.add('width-100')
+  trees.classList.add('width-100')
+  turnTable.classList.add('width-100')
+  mtn.classList.add('width-100')
+  rightButton.addEventListener('click',()=>{
+    mtn.style.display="block"
+    trees.style.display="block"
+    comp.style.display="none"
+    turnTable.style.display='none'
+
+  })
+  leftButton.addEventListener('click',()=>{
+     comp.style.display="block"
+     turnTable.style.display='block'
+     mtn.style.display="none"
+     trees.style.display="none"
+  })
+               
+  return carouselComponent,leftButton,rightButton,turnTable,comp,trees,mtn
+  }
+  carouselComponent()
+
+  
